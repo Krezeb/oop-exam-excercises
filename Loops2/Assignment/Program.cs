6ad5516f586bd6daba583b854d57a66b368bee40
Lachlan Morrison <lmorrison85@gmail.com>
2021-11-09 19:16:23 +0100

    Uppgift complete with compressed logic

--- Loops2/Assignment/Program.cs
+++ Loops2/Assignment/Program.cs
@@ -10,20 +10,12 @@ namespace Assignment
             for (int i = 0; i < myArray.Length; i++)
             {
                 myArray[i] = i + 1;
-
                 if (myArray[i] % 3 == 0 && myArray[i] % 5 == 0)
-                {
                     myArray[i] = myArray[i] * myArray[i - 1] + 15;
-                }
                 else if (myArray[i] %3 == 0)
-                {
                     myArray[i] = myArray[i] * myArray[i-1];
-                }
                 else if (myArray[i] %5 == 0)
-                {
                     myArray[i] = myArray[i] + 15;
-                }
-
             }
         }
     }
