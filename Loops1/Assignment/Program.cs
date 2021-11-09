2776551b79a50d07b92945f0c1032a3747599965
Lachlan Morrison <lmorrison85@gmail.com>
2021-11-09 18:00:51 +0100

    finished

--- Loops1/Assignment/Program.cs
+++ Loops1/Assignment/Program.cs
@@ -6,6 +6,37 @@ namespace Assignment
     {
         static void Main(string[] args)
         {
+            int siffra = 0;
+            int countTo = 100;
+
+            while (true)
+            {
+                Console.WriteLine("Ange en siffra: ");
+                siffra = Convert.ToInt32(Console.ReadLine());
+
+                if (siffra < 200 && siffra > -200)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Siffra för stor / Liten. Prova igen.");
+                }
+            }
+            if (siffra < 100)
+            {
+                for (int i = siffra; i <= countTo; i++)
+                {
+                    Console.WriteLine($"{i}");
+                }
+            }
+            else if (siffra > 100)
+            {
+                for (int i = siffra; i >= countTo; i--)
+                {
+                    Console.WriteLine($"{i}");
+                }
+            }
         }
     }
 }
