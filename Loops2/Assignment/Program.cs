8e86bc9332500d19ae359dfd92db5a904d8755e5
Lachlan Morrison <lmorrison85@gmail.com>
2021-11-09 19:28:01 +0100

    with both debugging and compressed code

--- Loops2/Assignment/Program.cs
+++ Loops2/Assignment/Program.cs
@@ -6,17 +6,43 @@ namespace Assignment
     {
         static void Main(string[] args)
         {
-            int[] myArray = new int[100];
-            for (int i = 0; i < myArray.Length; i++)
-            {
-                myArray[i] = i + 1;
-                if (myArray[i] % 3 == 0 && myArray[i] % 5 == 0)
-                    myArray[i] = myArray[i] * myArray[i - 1] + 15;
-                else if (myArray[i] %3 == 0)
-                    myArray[i] = myArray[i] * myArray[i-1];
-                else if (myArray[i] %5 == 0)
-                    myArray[i] = myArray[i] + 15;
-            }
+            //int[] myArray = new int[100];
+            //for (int i = 0; i < myArray.Length; i++)
+            //{
+            //    myArray[i] = i + 1;
+            //    if (myArray[i] % 3 == 0 && myArray[i] % 5 == 0)
+            //        myArray[i] = myArray[i] * myArray[i - 1] + 15;
+            //    else if (myArray[i] %3 == 0)
+            //        myArray[i] = myArray[i] * myArray[i-1];
+            //    else if (myArray[i] %5 == 0)
+            //        myArray[i] = myArray[i] + 15;
+            //}
+
+            //int[] myArray = new int[100];
+            //for (int i = 0; i < myArray.Length; i++)
+            //{
+            //    myArray[i] = i + 1;
+
+            //    if (myArray[i] % 3 == 0 && myArray[i] % 5 == 0)
+            //    {
+            //        myArray[i] = myArray[i] * myArray[i - 1] + 15;
+            //        Console.WriteLine($"if %3 + %5 - Index [{i}] = {myArray[i]}");
+            //    }
+            //    else if (myArray[i] % 3 == 0)
+            //    {
+            //        myArray[i] = myArray[i] * myArray[i - 1];
+            //        Console.WriteLine($"if      %3 - Index [{i}] = {myArray[i]}");
+            //    }
+            //    else if (myArray[i] % 5 == 0)
+            //    {
+            //        myArray[i] = myArray[i] + 15;
+            //        Console.WriteLine($"else if %5 - Index [{i}] = {myArray[i]}");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"else       - Index [{i}] = {myArray[i]}");
+            //    }
+            //}
         }
     }
 }
