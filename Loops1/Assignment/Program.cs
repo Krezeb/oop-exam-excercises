a30b3779158dfa997115269d4ac94697e353af0a
Krezeb <lachlanmorrison@outlook.com>
2021-11-10 18:21:52 +0100

    done

--- Loops1/Assignment/Program.cs
+++ Loops1/Assignment/Program.cs
@@ -6,6 +6,46 @@ namespace Assignment
     {
         static void Main(string[] args)
         {
+            int siffra = 0;
+            while (true)
+            {
+                Console.WriteLine(("Mata in ett siffra:"));
+                string input = Console.ReadLine();
+                try
+                {
+                    siffra = Convert.ToInt32(input);
+                    if (siffra < -200 || siffra > 200)
+                    {
+                        Console.WriteLine("Skriva ett nytt tal...");
+                        continue;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Ogiltig inmatning...");
+                    continue;
+                }
+            }
+
+            if (siffra > 100)
+            {
+                for (int i = siffra; i < 100; i--)
+                {
+                    Console.WriteLine($"Siffra: {i}");
+                }
+            }
+            else if (siffra < 100)
+            {
+                for (int i = 100; i < siffra; i++)
+                {
+                    Console.WriteLine($"Siffra: {i}");
+                }
+            }
+           
         }
     }
 }
