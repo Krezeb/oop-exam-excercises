129dae10bd1ffc18f522c8fdf4a73bd882a6fcbb
Lachlan Morrison <lmorrison85@gmail.com>
2021-11-10 13:24:47 +0100

    Notes and minor functionality added

--- Files1/Assignment/Program.cs
+++ Files1/Assignment/Program.cs
@@ -8,6 +8,12 @@ namespace Assignment
     {
         static void Main(string[] args)
         {
+            string inputGenre = "Programming"; //If Lines 14 + 15 are commented out, program will run as Assignment decrees with this variable.
+            string format = "{0,-30} {1,-20} {2}";
+
+            Console.WriteLine("Input Genre to list: ");
+            inputGenre = Console.ReadLine();
+
             string[] _bookList = File.ReadAllLines("books.txt");
             List<Book> _bookList2 = new List<Book>();
 
@@ -26,16 +32,16 @@ namespace Assignment
             }
             _bookList2.RemoveAt(1);
 
-            string format = "{0,-30} {1,-20} {2}";
             Console.WriteLine(format, _bookList2[0].Title, _bookList2[0].Author, _bookList2[0].Genre);
             foreach (Book book in _bookList2)
             {
-                if (book.Genre == "Programming")
+                if (book.Genre.ToLower() == inputGenre.ToLower())
                 {
                     string _title = book.Title;
                     if (_title.Length > 30)
                     {
-                        _title = book.Title.Substring(0, 29);
+                        _title = book.Title.Substring(0, 29); //0 is the starting letter, 29 is the last
+                        //book.Title[..29]; //Shortening strings can be done like this aswell
                     }
                     Console.WriteLine(format, _title, book.Author, book.Genre);
                 }
